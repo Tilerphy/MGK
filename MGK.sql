ac87d9839ea32ecc3777d1f48467ddba5691e1ac
root <root@homeserver.hs.org>
2016-04-12 13:26:45 +0800

    add example

--- MGK.sql
+++ MGK.sql
@@ -6,29 +6,40 @@ create table the_user(id int primary key auto_increment,
     the_career int,
     the_race int,
     the_union int,
-    the_place int
+    the_area int
 );
 
 create table the_attribute(id int primary key auto_increment,
     stength int,
     staying int,
     knowledge int,
-    business int,
+    speed int,
     hp int,
     mana int,
     crystal int,
-    ap int
+    ap int,
+    
+    business int,
+    speech int,
+    charm int,
+    insight int,
+    health int,
+    
 );
+
+--create table the_ex_attribute(id int primary key auto_increment,
+--    name varchar(16),
+--    val int
+--);
 
 create table the_effect(id int primary key auto_increment,
     name varchar(128),
     the_attribute_name varchar(16),
     percent int,
     raw_value int,
-    effect_level int
+    effect_level int,
+    reason varchar(16)
 );
-
-
 
 create table the_date(id int primary key auto_increment,
     x_date varchar(128);
@@ -47,11 +58,6 @@ create table the_users_actions(id int primary key auto_increment,
     name varchar(128),
 );
 
-create table the_area (id int primary key auto_increment,
-    name varchar(128),
-    the_date int,   
-);
-
 create table the_weather(id int primary key auto_increment,
     name varchar(128)
 );
@@ -59,17 +65,13 @@ create table the_race(id int primary key auto_increment,
     name varchar(128),
     desciption varchar(512)
 );
-create table the_place(id int primary key auto_increment,
+create table the_area(id int primary key auto_increment,
     name varchar(128),
-    the_area int,
     the_weather int
 );
 create table the_union(id int primary key auto_increment,
     name varchar(128),
     desciption varchar(512)
 );
-create table the_career(id int primary key auto_increment,
-    name varchar(128),
-    desciption varchar(512)
-);
+
 
